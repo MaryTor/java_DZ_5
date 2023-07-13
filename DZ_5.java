21f22f13f074c283ba8358fda9f84ce28f5cfac5
Mary Tor <of51@mail.ru>
2023-07-13 17:19:15 +0300

    Домашка 5

---
+++ DZ_5.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DZ_5 {
+  public static void main(String[] args) {
+    HashMap<String, String> book = new HashMap<>();
+    put(book, "Ivan", "+78737892387");
+    put(book, "Roma", "+85939878922");
+    put(book, "Ivan", "+93458992255");
+    put(book, "Lida", "+78745377321");
+    print(book);
+
+  }
+
+  public static void put(HashMap<String, String> book, String contact, String phone) {
+    book.merge(contact, phone, (ph1, ph2) -> ph1 + ", " + ph2);
+  }
+
+  public static void print(HashMap<String, String> book) {
+    ArrayList<Map.Entry<String, String>> list = new ArrayList<>(book.entrySet());
+    list.sort((o1, o2) -> o2.getValue().length() - o1.getValue().length());
+    for (Map.Entry<String, String> entry : list) {
+      System.out.println(entry.getKey() + ": " + entry.getValue());
+    }
+  }
+}
